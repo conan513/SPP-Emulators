023dcef5b1dc0ce38c5434eb936daf5ec04ab221
ConanHUN <conan_513@hotmail.com>
2021-01-22 18:13:11 +0100

    up yuzu

--- Nintendo-Switch/INSTALL_KEYS_FOR_YUZU.bat
+++ Nintendo-Switch/INSTALL_KEYS_FOR_YUZU.bat
@@ -1,10 +1,11 @@
 @echo off
 echo Installing default keys for the emulator...
 echo.
-mkdir %appdata%\yuzu\keys
-copy %CD%\keys\Prod.keys %appdata%\yuzu\keys
-copy %CD%\keys\title.keys %appdata%\yuzu\keys
+mkdir "%appdata%\yuzu\keys"
+del "%appdata%\yuzu\keys\*.keys"
+copy "%CD%\keys\Prod.keys" "%appdata%\yuzu\keys"
+copy "%CD%\keys\title.keys" "%appdata%\yuzu\keys"
 echo.
-echo Keys installed in %appdata%\yuzu\keys.
+echo Keys installed in "%appdata%\yuzu\keys".
 pause
 exit
